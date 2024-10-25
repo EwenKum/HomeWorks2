1d96da17363b3bfd535aef351085361e3fdeb44c
EwenKum <evgeniikumitskii@gmail.com>
2024-10-25 08:52:48 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Введите ваше имя: ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+my_string = input("Введите ваше имя: ".replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
